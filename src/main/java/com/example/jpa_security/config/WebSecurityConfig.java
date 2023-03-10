35d889e88fbaacb592c947049fec2c0479a1aa4e
ryu <ryjun91@gmail.com>
2023-03-10 11:00:26 +0900

    feat: 5. 비밀번호 암호화 이해하고 적용하기

--- src/main/java/com/example/jpa_security/config/WebSecurityConfig.java
+++ src/main/java/com/example/jpa_security/config/WebSecurityConfig.java
@@ -3,28 +3,28 @@ package com.example.jpa_security.config;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // 스프링 Security 지원을 가능하게 함
 public class WebSecurityConfig {
 
-    // 우선적으로 걸리는 설정이다.
+    @Bean // 비밀번호 암호화 기능 등록
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         // h2-console 사용 및 resources 접근 허용 설정
         return (web) -> web.ignoring()
                 .requestMatchers(PathRequest.toH2Console())
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-//        .antMatchers("/h2-console/**").permitAll()
-//                .antMatchers("/css/**").permitAll()
-//                .antMatchers("/js/**").permitAll()
-//                .antMatchers("/images/**").permitAll()
-//                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
     }
 
     @Bean
@@ -34,8 +34,6 @@ public class WebSecurityConfig {
 
         http.authorizeRequests().anyRequest().authenticated();
 
-        // 로그인 사용
-//        http.formLogin();
         // Custom 로그인 페이지 사용
         http.formLogin().loginPage("/api/user/login-page").permitAll();
 
